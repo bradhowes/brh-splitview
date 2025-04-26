0f897981c28436009eda71fc2087029e25f045bf
Brad Howes <bradhowes@mac.com>
2025-04-26 20:27:21 +0200

    Fixed minor bug and added commentary

--- Sources/brh-splitview/Extensions.swift
+++ Sources/brh-splitview/Extensions.swift
@@ -3,19 +3,47 @@
 import SwiftUI
 
 internal extension Comparable {
+
+  /**
+   Obtain a new value that is clamped to the given range.
+
+   - parameter limits: the range to use
+   - returns: new value
+   */
   func clamped(to limits: ClosedRange<Self>) -> Self { min(max(self, limits.lowerBound), limits.upperBound) }
 }
 
 internal extension Comparable where Self: BinaryFloatingPoint {
-  func normalize(in limits: ClosedRange<Self>) -> Self { clamped(to: limits) / limits.upperBound }
+
+  /**
+   Obtain a normalized value (0-1.0) over the given range.
+
+   - parameter limits: the range to use
+   - returns: normalized value
+   */
+  func normalize(in limits: ClosedRange<Self>) -> Self { clamped(to: limits) / (limits.upperBound  - limits.lowerBound)}
 }
 
 internal extension ClosedRange {
+
+  /**
+   Clamp the given value so that it is within our bounds.
+
+   - parameter value to clamp
+   - returns: new value
+   */
   func clamp(value : Bound) -> Bound { value.clamped(to: self) }
 }
 
 internal extension ClosedRange where Bound: BinaryFloatingPoint {
-  func normalize(value: Bound) -> Bound { value.clamped(to: self) / self.upperBound }
+
+  /**
+   Normalize the given value over our bounds.
+
+   - parameter value to normalize
+   - returns: normalized value
+   */
+  func normalize(value: Bound) -> Bound { value.normalize(in: self) }
 }
 
 extension EnvironmentValues {
